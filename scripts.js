34c720f6a178259981a81841409487d1c50b5901
Kuldeep Singh Gautam <82753722+kuldeepsinghgautam@users.noreply.github.com>
2024-08-09 08:45:05 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+
+ 
+    <script>
+    function myFunction(mpyDemo1) {
+      var x = document.getElementById(mpyDemo1);
+      if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+      } else { 
+        x.className = x.className.replace(" w3-show", "");  }}</script>
+    
+ 
+
+
+
+    <script>
+      function myFunction(mpyDemo2) {
+        var x = document.getElementById(mpyDemo2);
+        if (x.className.indexOf("w3-show") == -1) {
+          x.className += " w3-show";
+        } else { 
+          x.className = x.className.replace(" w3-show", "");  }}</script>
+      
+   
